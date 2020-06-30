9839411498741ccc3a6b9915f3494289f5c14447
rika0384 <sora6kuro0f9@yahoo.co.jp>
2020-06-30 17:21:26 +0900

    fix typo

--- main.js
+++ main.js
@@ -157,7 +157,7 @@ app.post("/submit",async(req,res)=>{
 	//insertを呼び出す
 	new Promise(async(resolve, reject) => {
 		await request({
-		    url: base_url_localhost + "/insert",
+		    contest_url: base_url_localhost + "/insert",
 		    method: 'POST',
 		    json:{"contest_url":req.body.contest_url,"writer":req.body.writer}
 		}, function (error, response, body) {
